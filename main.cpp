dfaf65d635c814a39b197ebc5e011e7464ef99bd
aiden cox <cox.378@wright.edu>
2025-03-05 14:33:52 -0500

    beginner c++ silly calculator

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
-
+#include <cmath>
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    double a;
+    double b;
+    double c;
+
+    std::cout << "Enter side A: ";
+    std::cin >> a;
+
+    std::cout << "Enter side B: ";
+    std::cin >> b;
+
+    a = pow(a, 2);
+    b = pow(b, 2);
+    c = sqrt(a + b);
+
+    std::cout << "Side C: " << c;
+
     return 0;
 }
